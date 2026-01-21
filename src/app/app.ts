eacc6637cedb4169f95430ac7b2d19aa9c19886e
veradebv <veradebora@outlook.co.id>
2026-01-21 13:19:56 +0700

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CounterComponent } from './counter/counter';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, CounterComponent], // 👈 allow counter
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular-beginner');
+  protected readonly title = signal('Discovera');
 }
