5b703a5c36336be243fac0d4a2ff59b5c7ede615
veradebv <veradebora@outlook.co.id>
2026-01-22 14:07:40 +0700

    updated reading status; keep shared state in services and update it through service methods, then reflect changes in components.

--- src/app/book-detail/book-detail.ts
+++ src/app/book-detail/book-detail.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { BookService, Book } from '../book.service';
+import { BookService, Book, ReadingStatus } from '../book.service';
 
 @Component({
   selector: 'app-book-detail',
@@ -18,5 +18,12 @@ export class BookDetailComponent {
   ) {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.book = this.bookService.getBookById(id);
+  }
+
+  setStatus(status: ReadingStatus) {
+    if (!this.book) return;
+    
+      this.bookService.updateStatus(this.book.id, status);
+      this.book.status = status;
   }
 }
--- src/app/book.service.ts
+++ src/app/book.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 
+export type ReadingStatus = 'want-to-read' | 'reading' | 'read';
+
 export interface Book {
   id: number;
   title: string;
   author: string;
   rating: number;
+  status?: ReadingStatus;
 }
 
 @Injectable({
@@ -12,10 +15,32 @@ export interface Book {
 })
 export class BookService {
   private books: Book[] = [
-    { id: 0, title: 'The Midnight Library', author: 'Matt Haig', rating: 4.2 },
-    { id: 1, title: 'Atomic Habits', author: 'James Clear', rating: 4.5 },
-    { id: 2, title: 'Norwegian Wood', author: 'Haruki Murakami', rating: 4.0 },
+    { 
+      id: 0, 
+      title: 'The Midnight Library', 
+      author: 'Matt Haig', 
+      rating: 4.2,
+      status: 'want-to-read'
+    },
+    { 
+      id: 1, 
+      title: 'Atomic Habits', 
+      author: 'James Clear', 
+      rating: 4.5,
+      status: 'reading'
+    },
+    { 
+      id: 2, 
+      title: 'Norwegian Wood', 
+      author: 'Haruki Murakami', 
+      rating: 4.0,
+      status: 'read'
+    },
   ];
+
+  updateStatus(id: number, status: ReadingStatus){
+    const book = this.books.find(book => book.id === id);
+  }
 
   getBooks(): Book[] {
     return this.books;
