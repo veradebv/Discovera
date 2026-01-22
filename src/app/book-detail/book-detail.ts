91ae3c8d7f3a21dd1635a475b8a0a85a8d287876
veradebv <veradebora@outlook.co.id>
2026-01-22 15:16:08 +0700

    added reviews to books

--- src/app/book-detail/book-detail.ts
+++ src/app/book-detail/book-detail.ts
@@ -1,19 +1,22 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { BookService, Book, ReadingStatus } from '../book.service';
 
 @Component({
   selector: 'app-book-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './book-detail.html',
 })
 export class BookDetailComponent {
   book?: Book;
+  newReview = '';
 
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private bookService: BookService
   ) {
     const id = Number(this.route.snapshot.paramMap.get('id'));
@@ -22,8 +25,18 @@ export class BookDetailComponent {
 
   setStatus(status: ReadingStatus) {
     if (!this.book) return;
-    
       this.bookService.updateStatus(this.book.id, status);
       this.book.status = status;
+  }
+
+  addReview() {
+    if (!this.book || !this.newReview.trim()) return;
+
+    this.bookService.addReview(this.book.id, this.newReview);
+    this.newReview = '';
+  }
+
+  goBack() {
+    this.router.navigate(['/']);
   }
 }
--- src/app/book.service.ts
+++ src/app/book.service.ts
@@ -2,12 +2,18 @@ import { Injectable } from '@angular/core';
 
 export type ReadingStatus = 'want-to-read' | 'reading' | 'read';
 
+export interface Review {
+  text: string;
+  createdAt: Date;
+}
+
 export interface Book {
   id: number;
   title: string;
   author: string;
   rating: number;
   status?: ReadingStatus;
+  reviews: Review[];
 }
 
 @Injectable({
@@ -20,27 +26,44 @@ export class BookService {
       title: 'The Midnight Library', 
       author: 'Matt Haig', 
       rating: 4.2,
-      status: 'want-to-read'
+      status: 'want-to-read',
+      reviews: []
     },
     { 
       id: 1, 
       title: 'Atomic Habits', 
       author: 'James Clear', 
       rating: 4.5,
-      status: 'reading'
+      status: 'reading',
+      reviews: []
     },
     { 
       id: 2, 
       title: 'Norwegian Wood', 
       author: 'Haruki Murakami', 
       rating: 4.0,
-      status: 'read'
+      status: 'read',
+      reviews: []
     },
   ];
 
   updateStatus(id: number, status: ReadingStatus){
     const book = this.books.find(book => book.id === id);
+    if (!book) return;
+  
+    book.status = status;
   }
+
+  addReview(bookId: number, text: string) {
+  const book = this.books.find(b => b.id === bookId);
+  if (!book) return;
+
+  book.reviews.push({
+    text,
+    createdAt: new Date(),
+  });
+}
+
 
   getBooks(): Book[] {
     return this.books;
